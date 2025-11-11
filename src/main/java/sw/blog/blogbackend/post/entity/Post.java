6f18039cfe9ef73677828469703931804de462b2
swkim <thearch90@gmail.com>
2025-11-11 17:12:38 +0900

    게시글 항목 및 기능 수정 작업중

--- src/main/java/sw/blog/blogbackend/post/entity/Post.java
+++ src/main/java/sw/blog/blogbackend/post/entity/Post.java
@@ -1,15 +1,21 @@
 package sw.blog.blogbackend.post.entity;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.data.annotation.CreatedDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -25,19 +31,33 @@ import lombok.Setter;
 @Table(name = "posts")
 public class Post {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column
+  private Long id;
 
-    @Column(nullable = false)
-    private String title;
+  @Column(nullable = false)
+  private String title;
 
-    @Lob
-    @Column(nullable = false, columnDefinition = "TEXT")
-    private String content;
+  @Lob
+  @Column(nullable = false, columnDefinition = "TEXT")
+  private String content;
 
-    @CreatedDate
-    @Column
-    private LocalDateTime createAt = LocalDateTime.now();
+  @CreatedDate
+  @Column
+  private LocalDateTime createAt = LocalDateTime.now();
+
+  @Column(nullable = false)
+  private String category;
+
+  @Column(name = "is_private", nullable = false)
+  private boolean isPrivate = false;
+
+  @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+  @JoinTable(name = "post_tags", joinColumns = @JoinColumn(name = "post_id"), inverseJoinColumns = @JoinColumn(name = "tag_id"))
+  private Set<Tag> tags = new HashSet<>();
+
+  public void addTag(Tag tag) {
+    this.tags.add(tag);
+  }
 }
---
+++ src/main/java/sw/blog/blogbackend/post/entity/Tag.java
@@ -0,0 +1,32 @@
+package sw.blog.blogbackend.post.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class Tag {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true, nullable = false, length = 50)
+  private String name;
+
+  private Tag(String name) {
+    this.name = name;
+  };
+}
