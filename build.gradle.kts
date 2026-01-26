970c59deb5438d397169b46581ed533602ea8cca
swkim <thearch90@gmail.com>
2026-01-26 14:56:17 +0900

    Jar 빌드 파일명 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,3 +46,7 @@ dependencies {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    archiveFileName.set("blog-backend-app.jar")
+}
